7e34c6097407761d7c316c57bfa06dad2027eba4
SantosMaycon <mayconalves.santos14@gmail.com>
2021-07-17 18:48:49 -0300

    add component week

---
+++ src/components/Week/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.span`
+  display: inline-block;
+  height: 3.2rem;
+  text-align: left;
+  font-size: 2.4rem;
+  font-weight: 400;
+  color: #848484;
+`
---
+++ src/utils/week.js
@@ -0,0 +1,9 @@
+export const week = {
+  0: 'Sunday',
+  1: 'Monday',
+  2: 'Tuesday',
+  3: 'Wednesday',
+  4: 'Thursday',
+  5: 'Friday',
+  6: 'Saturday'
+}
