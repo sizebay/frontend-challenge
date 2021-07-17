5d5531367165963862daba3644705b49e06ff491
SantosMaycon <mayconalves.santos14@gmail.com>
2021-07-17 20:05:23 -0300

    add page template

---
+++ src/templates/Pages/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  max-height: 100%;
+  background-color: #555555;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
