5bf43fe255dbcfa4e8f07c7c2f1d56bfc4ba583a
SantosMaycon <mayconalves.santos14@gmail.com>
2021-07-17 20:11:17 -0300

    add component modal and index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,45 @@
+import PageTemplate from 'templates/Pages'
+import Modal from 'components/Modal'
+
+import { useContext } from 'react'
+import { GlobalContext } from 'context/GlobalContext'
+
 export default function Home() {
-  return <h1>Olá Maycon</h1>
+  const {
+    todos,
+    setTodos,
+    filter,
+    setFilter,
+    search,
+    setSearch,
+    disabled,
+    setDisabled,
+    hidden,
+    setHidden,
+    value,
+    maxValue,
+    searchRef
+  } = useContext(GlobalContext)
+
+  return (
+    <PageTemplate>
+      <Modal
+        todos={todos}
+        setTodos={setTodos}
+        filter={filter}
+        setFilter={setFilter}
+        search={search}
+        setSearch={setSearch}
+        disabled={disabled}
+        setDisabled={setDisabled}
+        hidden={hidden}
+        setHidden={setHidden}
+        value={value}
+        maxValue={maxValue}
+        searchRef={searchRef}
+        done="Done"
+        pending="Pending"
+      />
+    </PageTemplate>
+  )
 }
