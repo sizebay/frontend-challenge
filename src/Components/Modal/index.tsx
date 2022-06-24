e727b70005acd194ac339c5f6484f6e246eca8e8
vinewz <vinewalves@hotmail.com>
2022-06-24 16:09:31 -0300

    Create todos filter function

--- src/Components/Modal/index.tsx
+++ src/Components/Modal/index.tsx
@@ -1,5 +1,6 @@
 
 import { AddNewTodoInput } from '../AddNewTodoInput';
+import { FilterTodos } from '../FilterTodos';
 import { Header } from '../Header';
 import { TodoList } from '../TodoList';
 import { ModalContainer } from './styles';
@@ -10,6 +11,7 @@ export function Modal() {
     return (
         <ModalContainer>
             <Header />
+            <FilterTodos />
             <AddNewTodoInput />
             <TodoList />
         </ModalContainer>
