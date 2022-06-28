c879939fdb61fa086a168b8e1ef97de16f6675dc
Vinicios <viniciossilva@cazamba.com>
2022-06-28 10:59:26 -0300

    Create input to add new tasks

--- src/components/AddNewTaskInput/index.tsx
+++ src/components/AddNewTaskInput/index.tsx
@@ -1,13 +1,21 @@
 import { PlusCircle } from "phosphor-react";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
+import { useTasks } from "../../contexts/TasksContext";
 import { Input, InputContainer, SubmitButton } from "./styles";
 
 export function AddNewTaskInput() {
 
+    const { createNewTask, completedTasks, pendingTasks } = useTasks()
     const [newItem, setNewItem] = useState('')
 
+    function addNewTask(e: FormEvent) {
+        e.preventDefault()
+        createNewTask(newItem)
+        setNewItem('')
+    }
+
     return (
-        <InputContainer>
+        <InputContainer onSubmit={(e) => addNewTask(e)}>
             <Input
                 type="text"
                 name="add new task"
@@ -17,6 +25,7 @@ export function AddNewTaskInput() {
             />
             <SubmitButton
                 disabled={!newItem}
+                type="submit"
             >
                 <PlusCircle size={26} weight='fill' color='white' />
             </SubmitButton>
