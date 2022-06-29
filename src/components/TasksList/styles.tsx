27e70965287d254daefc47d386465d1ecbb5f711
Vinicios <vinewalves@hotmail.com>
2022-06-28 23:09:47 -0300

    Style scroll bar

--- src/components/TasksList/styles.tsx
+++ src/components/TasksList/styles.tsx
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
+import { theme } from '../../globalStyles/theme';
 
 export const TaskListContainer = styled.div`
   width: 100%;
-  height: calc(100% - 210px);
+  height: calc(100% - 250px);
   overflow-x: hidden;
   overflow-y: auto;
   display: flex;
@@ -10,7 +11,21 @@ export const TaskListContainer = styled.div`
   align-items: center;
   gap: .5rem;
 
-  
+
+  /* width */
+  ::-webkit-scrollbar {
+    width: 8px;
+  }
+  /* Track */
+  ::-webkit-scrollbar-track {
+    background: ${theme.colors.gray100};
+  }
+  /* Handle */
+  ::-webkit-scrollbar-thumb {
+    background: ${theme.colors.gray500};
+    border-radius: 9px;
+  }
+
   @media (max-width: 800px) {
     height: calc(100% - 300px);
   }
