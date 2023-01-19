b906596e01e049170f974a48cbc6f136f7a68b8f
Marcus <marcusicaromc@gmail.com>
2023-01-19 20:41:15 -0300

    Finish confetti animation

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,8 +1,11 @@
 import ProgressBar from "react-bootstrap/ProgressBar";
+import Confetti from "react-confetti";
 
 import "../assets/styles/progressbar.css";
 
 function Progressbar(props) {
+  // const { width, height } = useWindowSize();
+
   let totalTodos = props.todos.length;
   function handleCompletedTodos() {
     let result = 0;
@@ -25,6 +28,17 @@ function Progressbar(props) {
         visuallyHidden
         className='progressbar'
       />
+
+      {now === 100 && (
+        <Confetti
+          width={200}
+          height={200}
+          numberOfPieces={50}
+          recycle={false}
+          confettiSource={{ x: 100, y: 75, w: 200, h: 200 }}
+          className='confetti'
+        />
+      )}
     </>
   );
 }
