3da8cbca946b2472a7991c9f4a0573d0aab31f80
Raphael-Soares <soaresphaell@gmail.com>
2023-01-19 18:55:26 -0300

    Font fix

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,8 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: "Roboto", sans-serif;
+    font-family: "Roboto", "sans-serif", "Arial";
+    
   }
  
 `;
