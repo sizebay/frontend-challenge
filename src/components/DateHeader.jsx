3221b29d7c5c0c143fe7631c49ee4dca02630b18
Raphael-Soares <soaresphaell@gmail.com>
2023-01-19 17:59:51 -0300

    Dates working

--- src/components/DateHeader.jsx
+++ src/components/DateHeader.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import {useState} from "react";
 import styled from "styled-components";
 
 const Header = styled.header`
@@ -53,18 +53,25 @@ const WeekDay = styled.div`
     font-size: 24px;
 `;
 function DateHeader() {
+    var [date, setDate] = useState(new window.Date());
+
+    const weekDay = date.toLocaleString("en-us", {weekday: "long"});
+    const month = date.toLocaleString("en-us", {month: "short"});
+    const day = date.getDate();
+    const year = date.getFullYear();
+
     return (
         <>
             <Header>
                 <Date>
-                    <Day>09</Day>
+                    <Day>{day}</Day>
                     <SideContainer>
-                        <Month>Jul</Month>
-                        <Year>2021</Year>
+                        <Month>{month}</Month>
+                        <Year>{year}</Year>
                     </SideContainer>
                 </Date>
 
-                <WeekDay>Wednesday</WeekDay>
+                <WeekDay>{weekDay}</WeekDay>
             </Header>
         </>
     );
