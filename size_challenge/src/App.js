ee6acff30e20a19d70e2542d7ca03bf4ddaf23d7
FidelissJr <andrefidelisjr@hotmail.com>
2023-01-21 00:22:36 -0300

    project started

---
+++ size_challenge/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TaskMenu from './layouts/TaskMenu';
+
+function App() {
+  return (
+    <div className="App">
+      <TaskMenu/>
+    </div>
+  );
+}
+
+export default App;
---
+++ size_challenge/src/layouts/TaskMenu.js
@@ -0,0 +1,11 @@
+import TaskMenuStyle from './../styles/taskMenu.module.css' 
+
+function TaskMenu(){
+    return(
+        <div className={TaskMenuStyle.main_container}>
+            <div className={TaskMenuStyle.sub_container}>Hello, World</div>
+        </div>
+    )
+}
+
+export default TaskMenu
