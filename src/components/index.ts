88077268522d5e46941e44c6fc43ab1d0e2e95f9
Elton Souza <elton.souza1515@gmail.com>
2023-01-24 00:43:46 -0300

    feat: organize components export

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { default as CreateTask } from "./CreateTask";
+export { default as DateTask } from "./DateTask";
+export { default as Filters } from "./Filters";
+export { default as ListTask } from "./ListTask";
+export { default as ProgressBar } from "./ProgressBar";
+export { default as Search } from "./Search";
