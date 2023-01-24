8962df7b3445ef87785896a08f8d55fcd21aa524
Elton Souza <elton.souza1515@gmail.com>
2023-01-24 00:41:52 -0300

    feat: theme colors and fonts

---
+++ src/styles/theme.ts
@@ -0,0 +1,23 @@
+import { DefaultTheme } from "styled-components";
+
+const theme: DefaultTheme = {
+  fonts: {
+    roboto: "Roboto, sans-serif",
+  },
+  colors: {
+    primary: " rgba(132, 132, 132, 1)",
+    green: "rgba(93, 226, 144, 1)",
+    red: "rgba(227, 79, 79, 1)",
+    blue: "rgba(77, 166, 179, 1)",
+    white: "#fff",
+    background: {
+      primary: "rgba(255, 255, 255, 1)",
+      secondary: "rgba(85, 85, 85, 1)",
+      tertiary: "rgba(228, 228, 228, 1)",
+      filter: "rgba(247, 247, 248, 1)",
+    },
+    border: "rgba(219, 219, 219, 1)",
+  },
+};
+
+export { theme };
