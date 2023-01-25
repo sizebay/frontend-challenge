3f7bfcd20815e916d26dc740a273f2879cb7803d
luizfbarbosa12 <l.nandoferbarbosa@gmail.com>
2023-01-25 16:31:05 -0300

    formatted global state

--- challenge-sizebay/src/Contexts/GlobalState.jsx
+++ challenge-sizebay/src/Contexts/GlobalState.jsx
@@ -1,24 +1,37 @@
-import React, {useState} from 'react'
-import {GlobalContext} from './GlobalContext'
+import React, { useState } from "react";
+import { GlobalContext } from "./GlobalContext";
 
 const GlobalState = (props) => {
-const [pending, setPending] = useState(['Tarefa pendente'])
-const [done, setDone] = useState([])
-const [listToShow, setListToShow] = useState('') 
-const [search, setSearch] = useState('')
-const [taskProgress, setTaskProgress] = useState([])
-const [percentagesArray, setPercentagesArray] = useState([])
+  const [pending, setPending] = useState(["Tarefa pendente"]);
+  const [done, setDone] = useState([]);
+  const [listToShow, setListToShow] = useState("");
+  const [search, setSearch] = useState("");
+  const [taskProgress, setTaskProgress] = useState([]);
+  const [percentagesArray, setPercentagesArray] = useState([]);
 
-    
-const states = {pending, done, listToShow, search,taskProgress, percentagesArray }
-const setters = {setPending, setDone, setListToShow, setSearch, setTaskProgress, setPercentagesArray}
-const data = {states, setters}
+  const states = {
+    pending,
+    done,
+    listToShow,
+    search,
+    taskProgress,
+    percentagesArray,
+  };
+  const setters = {
+    setPending,
+    setDone,
+    setListToShow,
+    setSearch,
+    setTaskProgress,
+    setPercentagesArray,
+  };
+  const data = { states, setters };
 
   return (
     <GlobalContext.Provider value={data}>
       {props.children}
     </GlobalContext.Provider>
-  )
-}
+  );
+};
 
 export default GlobalState;
