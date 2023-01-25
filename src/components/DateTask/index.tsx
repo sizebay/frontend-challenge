ea9149b2705a54adec5a16fbf7f47f97305263fa
Elton Souza <elton.souza1515@gmail.com>
2023-01-24 22:30:37 -0300

    refact: accessing array data according to division

--- src/components/DateTask/index.tsx
+++ src/components/DateTask/index.tsx
@@ -1,18 +1,20 @@
 import { format } from "date-fns";
 import { ContainerDate, Day, DayOfWeek, Header, Month, Year } from "./style";
 export default function DateTask() {
-  const date = format(new Date(), "dd-MMM-yyyy-EEEE").split("-");
+  const [day, month, year, week] = format(new Date(), "dd-MMM-yyyy-EEEE").split(
+    "-"
+  );
 
   return (
     <Header>
       <ContainerDate>
-        <Day>{date[0]}</Day>
+        <Day>{day}</Day>
         <div>
-          <Month>{date[1]}</Month>
-          <Year>{date[2]}</Year>
+          <Month>{month}</Month>
+          <Year>{year}</Year>
         </div>
       </ContainerDate>
-      <DayOfWeek>{date[3]}</DayOfWeek>
+      <DayOfWeek>{week}</DayOfWeek>
     </Header>
   );
 }
