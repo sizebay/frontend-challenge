a3e7a2d4ac6d2d28edb7925df259eb4fc53a9af0
felipel7 <felipelseven@gmail.com>
2023-02-25 11:41:32 -0300

    style(SearchBar): add chip buttons

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,5 +1,5 @@
 import { FormEvent } from 'react';
-import { IoMdSearch, IoIosClose } from 'react-icons/io';
+import { IoMdSearch, IoIosClose, IoMdCheckmark } from 'react-icons/io';
 
 import styles from './styles.module.scss';
 
@@ -9,14 +9,28 @@ function SearchBar() {
   }
 
   return (
-    <form className={styles.inputContainer} onSubmit={handleSubmit}>
-      <input type="text" placeholder="Search items" />
-      <label>
-        <button type="submit" aria-label="Search">
-          {/* <IoIosClose size={33} /> */}
-          <IoMdSearch size={23} />
-        </button>
-      </label>
+    <form className={styles.searchBarContainer} onSubmit={handleSubmit}>
+      <ul className={styles.chipsContainer}>
+        <li>
+          <button>Done</button>
+        </li>
+        <li>
+          <button className={styles.active}>
+            <IoMdCheckmark size={15} />
+            Pending
+          </button>
+        </li>
+      </ul>
+
+      <p className={styles.inputContainer}>
+        <input type="text" placeholder="Search items" />
+        <label>
+          <button type="submit" aria-label="Search">
+            {/* <IoIosClose size={33} /> */}
+            <IoMdSearch size={23} />
+          </button>
+        </label>
+      </p>
     </form>
   );
 }
