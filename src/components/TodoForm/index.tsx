0f35cdd3328b648a52b4132042b1feb741382cbc
felipel7 <felipelseven@gmail.com>
2023-02-25 12:35:38 -0300

    feat(TodoForm): save new todo item to the store

--- src/components/TodoForm/index.tsx
+++ src/components/TodoForm/index.tsx
@@ -1,17 +1,50 @@
-import { FormEvent } from 'react';
+import { ChangeEvent, FormEvent, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addTodo } from '../../store/todoSlice';
+import { generateId } from '../../utils/idGenerator';
 import { IoIosAddCircle } from 'react-icons/io';
 
 import styles from './styles.module.scss';
 
 function TodoForm() {
+  const [inputValue, setInputValue] = useState('');
+
+  const dispatch = useDispatch();
+
   function handleSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
+
+    if (inputValue)
+      dispatch(
+        addTodo({
+          id: Number(generateId.next().value),
+          text: inputValue,
+          done: false,
+        })
+      );
+
+    setInputValue('');
+  }
+
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    e.preventDefault();
+
+    const value = e.target.value;
+
+    if (value) {
+      setInputValue(value);
+    }
   }
 
   return (
     <form className={styles.todoFormContainer} onSubmit={handleSubmit}>
       <p>
-        <input type="text" placeholder="Add new item..." />
+        <input
+          onChange={handleChange}
+          placeholder="Add new item..."
+          type="text"
+          value={inputValue}
+        />
         <label>
           <button type="submit" aria-label="Submit">
             <IoIosAddCircle size={24.63} fill={'#fff'} />
