bdd8f929c805242cbe19f74c245a0f83131cf028
felipel7 <felipelseven@gmail.com>
2023-02-25 14:55:02 -0300

    feat(store): add update and delete functionality

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -1,12 +1,19 @@
 import { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import { selectTotalTodos } from '../../store/todoSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { IoIosCheckmarkCircle, IoMdRemoveCircle } from 'react-icons/io';
+import {
+  deleteTodo,
+  selectTotalTodos,
+  updateTodo,
+} from '../../store/todoSlice';
 import { Todo } from '../../@types';
 
 import styles from './styles.module.scss';
 
 function TodoList() {
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [activeTodoId, setActiveTodoId] = useState<number | null>(null);
+  const dispatch = useDispatch();
   const totalTodos = useSelector(selectTotalTodos);
 
   const updateTodos = () => {
@@ -19,10 +26,48 @@ function TodoList() {
 
   if (!todos) return;
 
+  function handleItemClick(id: number) {
+    setActiveTodoId(activeTodoId === id ? null : id);
+  }
+
+  function handleDelete(id: number) {
+    dispatch(deleteTodo(id));
+  }
+
+  function handleUpdate(todo: Todo) {
+    const updatedTodo = {
+      ...todo,
+      done: true,
+    };
+    dispatch(updateTodo(updatedTodo));
+  }
+
   return (
     <ul className={styles.todoListContainer}>
       {todos?.map(todo => (
-        <li key={todo.id}>{todo.text}</li>
+        <li
+          key={todo.id}
+          className={todo.id === activeTodoId ? styles.active : ''}
+          onClick={() => handleItemClick(todo.id)}
+        >
+          {todo.text}
+          {todo.id === activeTodoId && (
+            <p className={styles.controls}>
+              <button
+                className={styles.delete}
+                onClick={() => handleDelete(todo.id)}
+              >
+                <IoMdRemoveCircle size={24.63} fill={'#fff'} />
+              </button>
+              <button
+                className={styles.done}
+                onClick={() => handleUpdate(todo)}
+              >
+                <IoIosCheckmarkCircle size={24.63} fill={'#fff'} />
+              </button>
+            </p>
+          )}
+        </li>
       ))}
     </ul>
   );
--- src/store/todoSlice.ts
+++ src/store/todoSlice.ts
@@ -19,10 +19,23 @@ const slice = createSlice({
       state.todos.push(action.payload);
       localStorage.setItem('todos', JSON.stringify(state.todos));
     },
+    deleteTodo: (state, action: PayloadAction<number>) => {
+      state.todos = state.todos.filter(todo => todo.id !== action.payload);
+      localStorage.setItem('todos', JSON.stringify(state.todos));
+    },
+    updateTodo: (state, action: PayloadAction<Todo>) => {
+      const todoIndex = state.todos.findIndex(
+        todo => todo.id === action.payload.id
+      );
+      if (todoIndex !== -1) {
+        state.todos[todoIndex] = action.payload;
+        localStorage.setItem('todos', JSON.stringify(state.todos));
+      }
+    },
   },
 });
 
-export const { addTodo } = slice.actions;
+export const { addTodo, deleteTodo, updateTodo } = slice.actions;
 
 export const selectTotalTodos: Selector<RootState, Todo[]> = createSelector(
   (state: RootState) => state.todos,
