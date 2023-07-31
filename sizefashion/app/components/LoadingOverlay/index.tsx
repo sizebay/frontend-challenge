5e87bdfc074c37c394237a6964be1ced6c809bb8
Mateus <mateusbdesousa@gmail.com>
2023-07-31 11:25:11 -0300

    components - loadingoverlay

---
+++ sizefashion/app/components/LoadingOverlay/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { FaSpinner } from 'react-icons/fa';
+
+interface IProps {
+  fullscreen?: boolean;
+}
+
+const LoadingOverlay = ({ fullscreen }: IProps): React.ReactNode => {
+
+  const overlayClass = `${fullscreen ? 'inset-0' : 'relative top-0 left-0 right-0 bottom-0'} flex items-center justify-center bg-blue-200 z-50 h-full`;
+
+  return (
+    <div className={overlayClass}>
+      <FaSpinner className="text-white animate-spin" size={40} />
+    </div>
+  );
+};
+
+export default LoadingOverlay;
