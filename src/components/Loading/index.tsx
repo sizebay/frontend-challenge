70230d8d15733ac882547ee3107a59e665877f0c
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2023-08-15 17:17:55 -0300

    feat: add loading client component

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import { Spinner } from '@nextui-org/react';
+import React from 'react';
+
+export const Loading = () => {
+  return <Spinner label="Loading..." color="default" />;
+};
