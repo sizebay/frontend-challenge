41e94bfb2253e3a5d62e485f7583c0e0c9c115e3
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2023-08-16 01:25:17 -0300

    feat: creating the mocks home component

---
+++ src/mocks/index.ts
@@ -0,0 +1,41 @@
+export const dataHome = {
+  title: 'SizeFashion',
+  subTitle: 'apresenta!',
+  description: [
+    'A SizeFashion está animada para compartilhar os destaques de produtos de nossas incríveis coleções neste grande evento.',
+    'Nosso web app foi desenvolvido pensando em facilitar a visualização e exploração das coleções, proporcionando uma experiência intuitiva e eficiente.'
+  ],
+  features: {
+    description: [
+      {
+        title: 'Destaques da coleção',
+        description:
+          'Apresentamos uma seleção cuidadosa dos produtos mais impressionantes de cada coleção, para que você possa ter uma ideia rápida do que temos a oferecer.'
+      },
+      {
+        title: 'Navegação fácil',
+        description:
+          'Com uma interface amigável, você poderá percorrer as diferentes coleções e explorar os produtos de forma simples e organizada.'
+      },
+      {
+        title: 'Detalhes dos produtos',
+        description:
+          'Cada produto será acompanhado por suas informações, como descrição, preço, disponibilidade de tamanhos e cores, para ajudar na apresentação.'
+      },
+      {
+        title: 'Favoritos',
+        description:
+          'Você poderá criar uma lista de favoritos, onde poderá salvar os produtos que mais chamarem sua atenção para facilitar o acesso posteriormente.'
+      },
+      {
+        title: 'Compartilhamento',
+        description:
+          'Você poderá compartilhar os produtos que mais gostar com seus amigos e familiares, para que eles também possam conhecer nossas coleções.'
+      }
+    ]
+  },
+  footer:
+    'Sempre comprometidos em fornecer a você uma experiência agradável e prática ao explorar e apresentar as coleções da SizeFashion. Fique à vontade para entrar em contato caso precise de qualquer suporte adicional. Aproveite o evento e aproveite as maravilhosas coleções que temos para oferecer!'
+};
+
+export type DataHome = typeof dataHome;
