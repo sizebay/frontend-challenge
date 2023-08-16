c5b0d3531c1165ed958b1b1455c681c86b85cec4
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2023-08-16 20:36:05 -0300

    test: create footer test

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import { Footer } from '.';
 
-describe('Footer', () => {
+describe('Footer component', () => {
   test('renders Sizebay logo', () => {
-    render(<Footer />);
-    const logoElement = screen.getByAltText('Sizebay logo');
-    expect(logoElement).toBeInTheDocument();
+    const { getByAltText } = render(<Footer />);
+    const logo = getByAltText('Sizebay logo');
+    expect(logo).toBeInTheDocument();
+  });
+
+  test('renders front-end challenge text', () => {
+    const { getByText } = render(<Footer />);
+    const challengeText = getByText('Front-end challenge Sizebay');
+    expect(challengeText).toBeInTheDocument();
   });
 
   test('renders copyright text', () => {
-    render(<Footer />);
-    const copyrightText = screen.getByText(/copyright © Samuel Ramos/i);
+    const { getByText } = render(<Footer />);
+    const copyrightText = getByText('copyright © Samuel Ramos');
     expect(copyrightText).toBeInTheDocument();
   });
 });
