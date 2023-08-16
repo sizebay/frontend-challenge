fbaf172707cea6751381032cc4e9520c0c2693de
Samuel Ramos <92615688+samuelrms@users.noreply.github.com>
2023-08-16 15:27:01 -0300

    test: creating the first tests

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,8 @@
 module.exports = {
-  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
-  testEnvironment: 'jest-environment-jsdom',
-  moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
-  },
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest'
-  }
-};
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  modulePaths: ['<rootDir>/src/']
+}
---
+++ src/components/Footer/Footer.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+
+import { Footer } from '.';
+
+describe('Footer', () => {
+  test('renders Sizebay logo', () => {
+    render(<Footer />);
+    const logoElement = screen.getByAltText('Sizebay logo');
+    expect(logoElement).toBeInTheDocument();
+  });
+
+  test('renders copyright text', () => {
+    render(<Footer />);
+    const copyrightText = screen.getByText(/copyright © Samuel Ramos/i);
+    expect(copyrightText).toBeInTheDocument();
+  });
+});
