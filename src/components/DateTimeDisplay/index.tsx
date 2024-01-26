1326b367c1673fd9a1ab6632bece8b59becf1317
mayoliveii <mayoliveirablv@gmail.com>
2024-01-26 20:47:33 -0300

    feat: add `DateTimeDisplay` component

---
+++ src/components/DateTimeDisplay/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { format } from 'date-fns';
+import {
+  DateTimeDisplayContainer,
+  DateContainer,
+  LargeText,
+  SmallText,
+  DayOfWeek,
+} from './style';
+
+function DateTimeDisplay() {
+  const currentDate = new Date();
+  const dayOfWeek = format(currentDate, 'EEEE');
+  const dayOfMonth = format(currentDate, 'dd');
+  const month = format(currentDate, 'MMM');
+  const year = format(currentDate, 'yyyy');
+
+  return (
+    <DateTimeDisplayContainer>
+      <LargeText>{dayOfMonth}</LargeText>
+      <DateContainer>
+        <SmallText>{month}</SmallText>
+        <SmallText>{year}</SmallText>
+      </DateContainer>
+      <DayOfWeek>{dayOfWeek}</DayOfWeek>
+    </DateTimeDisplayContainer>
+  );
+}
+
+export default DateTimeDisplay;
---
+++ src/components/DateTimeDisplay/style.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const DateTimeDisplayContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #848484;
+`;
+
+export const DateContainer = styled.div`
+  text-align: left;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
+export const LargeText = styled.div`
+  font-size: 60px;
+`;
+
+export const SmallText = styled.div`
+  font-size: 24px;
+`;
+
+export const DayOfWeek = styled.div`
+  margin-left: auto;
+  font-size: 24px;
+`;
