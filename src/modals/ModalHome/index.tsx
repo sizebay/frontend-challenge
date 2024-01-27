6e77486a0ea743af9c00685edb985ae1bc498c4d
mayoliveii <mayoliveirablv@gmail.com>
2024-01-27 10:51:35 -0300

    feat: save list items to localStorage

--- src/modals/ModalHome/index.tsx
+++ src/modals/ModalHome/index.tsx
@@ -4,21 +4,28 @@ import DateTimeDisplay from "../../components/DateTimeDisplay";
 import ProgressBar from "../../components/ProgressBar";
 import TaskStatusControls from "../../components/TaskStatusControls";
 import NewItemBar from "../../components/NewItemBar";
-import ItemList from "../../components/ItemList";
+import ItemList from "../../components/ListItems";
 
 function SquareComponent() {
-  const [items, setItems] = useState<string[]>([]);
+  const [items, setItems] = useState<string[]>(() => {
+    const storedItems = localStorage.getItem("items");
+    return storedItems ? JSON.parse(storedItems) : [];
+  });
+
   const totalTasks = 5;
   const completedTasks = 2;
 
   const handleAddItemClick = (item: string) => {
-    setItems([...items, item]);
+    const newItems = [...items, item];
+    setItems(newItems);
+    localStorage.setItem("items", JSON.stringify(newItems));
   };
 
   const handleDeleteItem = (index: number) => {
     const newItems = [...items];
     newItems.splice(index, 1);
     setItems(newItems);
+    localStorage.setItem("items", JSON.stringify(newItems));
   };
 
   return (
