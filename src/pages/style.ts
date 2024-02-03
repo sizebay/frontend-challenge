9e4ca9bbd6f595ea505153b4890e8426fdaabb5d
luc0liv <luoliveiradacunha@gmail.com>
2024-02-02 23:39:04 -0300

    feat: create Home page

---
+++ src/pages/style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+
+export const HomePage = styled.section`
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
