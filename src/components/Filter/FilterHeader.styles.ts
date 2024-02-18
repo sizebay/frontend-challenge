5931247f95c0fe6ad3a4fda7634f66b52b49472b
Rafael <rafael-brittes@hotmail.com>
2024-02-18 16:08:24 -0300

    feat: added filter buttons and input

---
+++ src/components/Filter/FilterHeader.styles.ts
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+import defaultTheme from "../../styles/themes/default";
+
+export const FilterHeaderContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 27px 0px 24px 0px;
+    width: 100%;
+
+    @media (max-width: 768px) {
+    padding: 24px 0px 16px 0px;
+    }
+`;
+
+export const ButtonContainer = styled.div`
+    display: flex;
+    gap: 8px;
+    justify-content: flex-end; 
+    @media (max-width: 768px) {
+        align-self: flex-end;
+        padding-top: 11px;
+    }
+`;
+
+export const FilterContainer = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+
+    @media (max-width: 768px) {
+        flex-direction: column-reverse;
+    }
+`;
+
+export const InputContainer = styled.div`
+    position: relative;
+    width: 100%; 
+`;
+
+export const Input = styled.input`
+    flex-grow: 1;
+    height: 40px;
+    border: 1px solid ${defaultTheme.gray[400]};
+    border-radius: 4px;
+    padding-left: 16.29px;
+    width: 100%;
+    max-width: 450px;
+    float: inline-end;
+    @media (max-width: 768px) {
+        max-width: 100%;
+        margin-right: 0;
+    }
+`;
---
+++ src/components/Filter/FilterHeader.tsx
@@ -0,0 +1,84 @@
+import React, { useEffect } from 'react';
+import { Button } from '../Button/Button';
+import { doneSelectTheme, doneTheme } from '../Button/Button.styles';
+import { Check, MagnifyingGlass, X } from '@phosphor-icons/react';
+import { FilterHeaderContainer, ButtonContainer, FilterContainer, InputContainer, Input } from './FilterHeader.styles';
+
+interface Task {
+    id: number;
+    completed: boolean;
+    taskText: string;
+}
+
+interface FilterHeaderProps {
+    tasks: Task[];
+    setFilteredTasks: (filteredTasks: Task[]) => void;
+    changeFilterButtons: (buttonName: 'done' | 'pending') => void;
+    filterInfos: {
+        done: boolean;
+        pending: boolean;
+        filterText: string;
+    };
+    setFiltersState: (filtersInfo: {
+        done: boolean;
+        pending: boolean;
+        filterText: string;
+    }) => void;
+}
+
+export function FilterHeader({ tasks, setFilteredTasks, changeFilterButtons, filterInfos, setFiltersState }: FilterHeaderProps) {
+    function filterTasks(): Task[] {
+        return tasks.filter(task => {
+            const isDone = filterInfos.done ? task.completed : true;
+            const isPending = filterInfos.pending ? !task.completed : true;
+            const searchText = filterInfos.filterText.toLowerCase();
+            const taskText = task.taskText.toLowerCase();
+            return isDone && isPending && taskText.includes(searchText);
+        });
+    }
+
+    function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+        setFiltersState({ ...filterInfos, filterText: event.target.value });
+    }
+
+    function handleClearFilters() {
+        setFiltersState({ done: false, pending: false, filterText: '' });
+    }
+
+    useEffect(() => {
+        const filteredTasks = filterTasks();
+        setFilteredTasks(filteredTasks);
+    }, [filterInfos]);
+
+    return (
+        <FilterHeaderContainer>
+            <FilterContainer>
+                <ButtonContainer>
+                    <Button
+                        theme={filterInfos.done ? doneSelectTheme : doneTheme}
+                        onButtonClick={() => changeFilterButtons('done')}>
+                        {filterInfos.done ? <Check /> : null} Done
+                    </Button>
+                    <Button
+                        theme={filterInfos.pending ? doneSelectTheme : doneTheme}
+                        onButtonClick={() => changeFilterButtons('pending')}>
+                        {filterInfos.pending ? <Check /> : null} Pending
+                    </Button>
+                </ButtonContainer>
+                <InputContainer>
+                    <Input
+                        type="text"
+                        placeholder="Search items"
+                        value={filterInfos.filterText}
+                        onChange={handleInputChange}
+                    />
+                    {(!filterInfos.done && !filterInfos.pending && filterInfos.filterText === "") ?
+                        <MagnifyingGlass size={17.49} weight="bold" style={{ position: 'absolute', right: '13.51px', top: '50%', transform: 'translateY(-50%)' }} /> :
+                        <X onClick={handleClearFilters} size={17.49} weight="bold" style={{ position: 'absolute', right: '13.51px', top: '50%', transform: 'translateY(-50%)' }} />
+                    }
+                </InputContainer>
+
+            </FilterContainer>
+        </FilterHeaderContainer>
+    );
+}
