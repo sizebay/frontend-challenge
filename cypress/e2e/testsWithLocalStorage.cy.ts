f3ac9e8c272be0614db35046d74c061c31f47491
Rafael <rafael-brittes@hotmail.com>
2024-02-18 21:16:53 -0300

    test: added interface tests

---
+++ cypress/e2e/testsWithLocalStorage.cy.ts
@@ -0,0 +1,123 @@
+describe('Tests on app open', () => {
+  beforeEach(() => {
+    const tasks = [
+      {
+        id: 1,
+        completed: true,
+        taskText: 'Task in localstorage.',
+      },
+      {
+        id: 2,
+        completed: false,
+        taskText: 'Another task in localstorage as well.',
+      },
+    ];
+
+    const tasksJSON = JSON.stringify(tasks);
+    localStorage.setItem('tasks', tasksJSON);
+  });
+
+  afterEach(() => {
+    localStorage.removeItem('tasks');
+  });
+
+  it('check colected tasks from localstorage', () => {
+    cy.visit("http://localhost:3024")
+
+    cy.get('[data-cy="taskList"]').children().its('length').should('eq', 2);
+    cy.get('[data-cy="progressBar"]').then($progressBar => {
+      const valorAtual = $progressBar[0].ariaValueNow;
+      cy.wrap(null).should(() => {
+        expect(valorAtual).to.be.eq('50');
+      });
+    });
+  })
+
+  it('check filters empty', () => {
+    cy.visit("http://localhost:3024")
+
+    cy.get('[data-cy="buttonContainer"] > :nth-child(1)').find('svg').should('not.exist');
+    cy.get('[data-cy="buttonContainer"] > :nth-child(2)').find('svg').should('not.exist');
+
+    cy.get('[data-cy="searchTaskInput"]').should('be.empty')
+    cy.get('[data-cy="newItemInput"]').should('be.empty')
+    cy.get('[data-cy="newItemInput"]').should('not.be.disabled')
+  })
+
+  it('check filters change', () => {
+    cy.visit("http://localhost:3024")
+
+    cy.get('[data-cy="buttonContainer"] > :nth-child(1)').click();
+    cy.get('[data-cy="buttonContainer"] > :nth-child(1)').find('svg').should('exist');
+    cy.get('[data-cy="newItemInput"]').should('be.disabled')
+    cy.get('[data-cy="taskList"]').children().its('length').should('eq', 1);
+
+    cy.get('[data-cy="buttonContainer"] > :nth-child(2)').click();
+    cy.get('[data-cy="buttonContainer"] > :nth-child(2)').find('svg').should('exist');
+    cy.get('[data-cy="newItemInput"]').should('be.disabled')
+    cy.get('[data-cy="newItemInput"]').should('be.disabled')
+    cy.get('[data-cy="taskList"]').children().its('length').should('eq', 1);
+
+    cy.get('[data-cy="searchTaskInput"]').type("This task do not exist!")
+    cy.get('[data-cy="newItemInput"]').should('be.disabled')
+    cy.get('[data-cy="notFoundMessage"]').contains('There are no items marked as pending. Clean the filter here to see all items.').should('exist');
+    cy.get('[data-cy="notFoundMessage"] > span').click();
+    cy.get('[data-cy="notFoundMessage"]').should('not.exist');
+    cy.get('[data-cy="newItemInput"]').should('not.be.disabled')
+    cy.get('[data-cy="taskList"]').children().its('length').should('eq', 2);
+  })
+
+  it('check create new task', () => {
+    cy.visit("http://localhost:3024")
+
+    cy.get('[data-cy="newItemInput"]').type('Brand new task!');
+    cy.get('[data-cy="newItemInput"]').should('have.value', 'Brand new task!');
+    cy.get('[data-cy="newItemInput"]').next().click();
+    cy.get('[data-cy="taskList"]').children().its('length').should('eq', 3);
+
+    cy.get('[data-cy="newItemInput"]').type('Another task!');
+    cy.get('[data-cy="newItemInput"]').next().click();
+    cy.get('[data-cy="taskList"]').children().its('length').should('eq', 4);
+
+    cy.get('[data-cy="progressBar"]').then($progressBar => {
+      const valorAtual = $progressBar[0].ariaValueNow;
+      cy.wrap(null).should(() => {
+        expect(valorAtual).to.be.eq('25');
+      });
+    });
+  })
+
+  it('check edit task', () => {
+    cy.visit("http://localhost:3024")
+
+    cy.wait(150)
+    cy.get('[data-cy="progressBar"]').then($progressBar => {
+      const valorAtual = $progressBar[0].ariaValueNow;
+      cy.wrap(null).should(() => {
+        expect(valorAtual).to.be.eq('50');
+      });
+    });
+    cy.get('[data-cy="openTask2"]').click()
+    cy.get('[data-cy="editTaskId2"] > :nth-child(2)').children().eq(1).click();
+
+    cy.get('[data-cy="progressBar"]').then($progressBar => {
+      const valorAtual = $progressBar[0].ariaValueNow;
+      cy.wrap(null).should(() => {
+        expect(valorAtual).to.be.eq('100');
+      });
+    });
+
+    cy.get('[data-cy="editTaskId2"] > :nth-child(2)').children().eq(1).click();
+    cy.get('[data-cy="progressBar"]').then($progressBar => {
+      const valorAtual = $progressBar[0].ariaValueNow;
+      cy.wrap(null).should(() => {
+        expect(valorAtual).to.be.eq('50');
+      });
+    });
+
+    // cy.get('[data-cy="openTask2"]').click()
+    cy.get('[data-cy="inputEditTaskId2"]').type("{backspace}, but editted.")
+    cy.get('[data-cy="modalContent"]').click()
+    cy.get('[data-cy="openTask2"]> :nth-child(1)' ).contains('Another task in localstorage as well, but editted.').should('exist')
+  })
+})
---
+++ cypress/e2e/testsWithoutLocalStorage.cy.ts
@@ -0,0 +1,74 @@
+import { format } from 'date-fns';
+
+const today = new Date();
+const formattedDate = format(today, 'dd MMM yyyy EEEE');
+const [day, month, year, dayOfWeek] = formattedDate.split(' ');
+
+describe('Tests on app open', () => {
+
+  it('check today dates', () => {
+    cy.visit("http://localhost:3024")
+
+    cy.get('[data-cy="day"]').should('contain', day)
+    cy.get('[data-cy="month"]').should('contain', month)
+    cy.get('[data-cy="year"]').should('contain', year)
+    cy.get('[data-cy="dayOfWeek"]').should('contain', dayOfWeek)
+  })
+
+  it('check filters empty', () => {
+    cy.visit("http://localhost:3024")
+
+    cy.get('[data-cy="buttonContainer"] > :nth-child(1)').find('svg').should('not.exist');
+    cy.get('[data-cy="buttonContainer"] > :nth-child(2)').find('svg').should('not.exist');
+
+    cy.get('[data-cy="searchTaskInput"]').should('be.empty')
+    cy.get('[data-cy="newItemInput"]').should('be.empty')
+    cy.get('[data-cy="newItemInput"]').should('not.be.disabled')
+  })
+
+  it('check filters change', () => {
+    cy.visit("http://localhost:3024")
+
+    cy.get('[data-cy="buttonContainer"] > :nth-child(1)').click();
+    cy.get('[data-cy="buttonContainer"] > :nth-child(1)').find('svg').should('exist');
+    cy.get('[data-cy="newItemInput"]').should('be.disabled')
+    cy.get('[data-cy="notFoundMessage"]').contains('There are no items marked as done. Clean the filter here to see all items.').should('exist');
+    cy.get('[data-cy="notFoundMessage"] > span').click();
+    cy.get('[data-cy="buttonContainer"] > :nth-child(1)').find('svg').should('not.exist');
+    cy.get('[data-cy="newItemInput"]').should('not.be.disabled')
+
+    cy.get('[data-cy="buttonContainer"] > :nth-child(2)').click();
+    cy.get('[data-cy="buttonContainer"] > :nth-child(2)').find('svg').should('exist');
+    cy.get('[data-cy="newItemInput"]').should('be.disabled')
+    cy.get('[data-cy="notFoundMessage"]').contains('There are no items marked as pending. Clean the filter here to see all items.').should('exist');
+    cy.get('[data-cy="notFoundMessage"] > span').click();
+    cy.get('[data-cy="buttonContainer"] > :nth-child(2)').find('svg').should('not.exist');
+    cy.get('[data-cy="newItemInput"]').should('not.be.disabled')
+
+    cy.get('[data-cy="searchTaskInput"]').type("This task do not exist!")
+    cy.get('[data-cy="newItemInput"]').should('be.disabled')
+    cy.get('[data-cy="notFoundMessage"]').contains('Your search found no results. Clean the search here to see all items.').should('exist');
+    cy.get('[data-cy="notFoundMessage"] > span').click();
+    cy.get('[data-cy="notFoundMessage"]').should('not.exist');
+    cy.get('[data-cy="newItemInput"]').should('not.be.disabled')
+
+    cy.get('[data-cy="searchTaskInput"]').type("This task do not exist!")
+    cy.get('[data-cy="notFoundMessage"]').should('exist');
+    cy.get('[data-cy="clearFiltersIcon"]').click();
+    cy.get('[data-cy="notFoundMessage"]').should('not.exist');
+  })
+
+  it('check create new task', () => {
+    cy.visit("http://localhost:3024")
+
+    cy.get('[data-cy="newItemInput"]').type('Brand new task!');
+    cy.get('[data-cy="newItemInput"]').should('have.value', 'Brand new task!');
+    cy.get('[data-cy="newItemInput"]').next().click();
+    cy.get('[data-cy="taskList"]').children().its('length').should('eq', 1);
+
+    cy.get('[data-cy="newItemInput"]').type('Another task!');
+    cy.get('[data-cy="newItemInput"]').next().click();
+    cy.get('[data-cy="taskList"]').children().its('length').should('eq', 2);
+
+  })
+})
