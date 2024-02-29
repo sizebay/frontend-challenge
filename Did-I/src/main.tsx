0981d07afd1d43aa62944faa29a30b33a7f60b5f
Pedro Assuncao <phap9060@gmail.com>
2024-02-28 23:41:58 -0300

    Changing names to pattern

--- Did-I/src/main.tsx
+++ Did-I/src/main.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { ThemeProvider } from "styled-components";
-import { primaryTheme } from "./Themes/primary.style.ts";
-import { GlobalStyle } from "./globalStyle.ts";
+import { primaryTheme } from "./Themes/primary.theme.ts";
+import { GlobalStyle } from "./Themes/globalStyle.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
