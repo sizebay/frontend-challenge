d91da2cb4cdafad1e8b50abf89db76031d4f3e8e
gustapine <gustavohd34@gmail.com>
2024-03-07 01:12:30 -0300

    feat: progress bar

--- src/components/ProgressBar/index.tsx
+++ src/components/ProgressBar/index.tsx
@@ -1,4 +1,7 @@
+import React, { useEffect, useState } from "react";
 import { linearProgressClasses, styled, LinearProgress } from "@mui/material";
+import { useTasksContext } from "../../context/TasksContext";
+
 
 const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
   height: 24,
@@ -14,6 +17,18 @@ const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
   },
 }));
 
-export default function ProgressBar() {
-  return <BorderLinearProgress variant="determinate" value={60} />;
-}
+const ProgressBar = () => {
+  const { tasks } = useTasksContext();
+  const [divide, setDivide] = useState(0);
+
+  useEffect(() => {
+    const totalTasks = tasks.length;
+    const completedTasks = tasks.filter((task) => task.isCompleted).length;
+    const percentage =
+      totalTasks === 0 ? 0 : (completedTasks / totalTasks) * 100;
+    setDivide(percentage);
+  }, [tasks]);
+  return <BorderLinearProgress variant="determinate" value={divide} />;
+};
+
+export default ProgressBar;
